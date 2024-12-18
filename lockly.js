e283606900813190d4305bd5e615e3f5507c72c5
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-18 10:07:38 -0600

    Update lockly.js

--- lockly.js
+++ lockly.js
@@ -14,7 +14,27 @@ crypto.subtle
       Array.from(new Uint8Array(e))
         .map((e) => e.toString(16).padStart(2, "0"))
         .join("") &&
-      (
-        // Register sevice worker and redirect to the main page
+      (     
+        if ("serviceWorker" in navigator) {
+          window.addEventListener("load", function() {
+            navigator.serviceWorker
+            .register("/locklySW.js")
+            .then(res => {        
+              console.log("service worker registered", res);      
+                window.serviceWorkerRegistration = res;
+            })
+            .catch(err => console.log("service worker not registered", err))
+          })
+        }
+        
+        window.rebootServiceWorker = () => {
+          if (navigator.onLine) {
+            serviceWorkerRegistration.unregister();
+            localStorage.clear();
+            window.location.reload();
+          } else {
+            console.error('Client is not online or "navigator.onLine" is not availible');
+          }
+        }
       );
   });
