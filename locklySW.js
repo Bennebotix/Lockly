4bad2ad753034b45adae5b7eadfdbb243c1241bd
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-18 12:31:59 -0600

    Update locklySW.js

--- locklySW.js
+++ locklySW.js
@@ -2,7 +2,7 @@ self.addEventListener("fetch", (event) => {
   event.waitUntil(
     clients.matchAll().then(clients => {
       clients.forEach(client => {
-        client.postMessage(`Request URL: ${event.request.url}`);
+        client.postMessage(`"${event.request.host}" vs. "${window.location.host}"`);
       });
     })
   );
