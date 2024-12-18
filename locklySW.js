a3c46d962faf49d51fa6532be9dfa9c6d4564f4b
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-18 12:36:11 -0600

    Update locklySW.js

--- locklySW.js
+++ locklySW.js
@@ -1,4 +1,8 @@
 self.addEventListener("fetch", (event) => {
+  client.postMessage('Service Worker Registered!');
+}
+
+self.addEventListener("fetch", (event) => {
   event.waitUntil(
     clients.matchAll().then(clients => {
       clients.forEach(client => {
