5ce24420c9abeb169d38485262312a510e41cbc3
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-19 10:06:37 -0600

    Update locklySW.js

--- locklySW.js
+++ locklySW.js
@@ -1,12 +1,17 @@
-addEventListener("fetch", (event) => {
-  // Check if the request is for HTML
-  const isHtmlRequest = event.request.headers.get("Accept")?.includes("text/html");
-  
-  if (isHtmlRequest) {
-    // Respond with 'test' if it's an HTML request
-    event.respondWith(new Response("test", { status: 200 }));
-  } else {
-    // Handle other request types here if necessary
-    event.respondWith(new Response("Not HTML", { status: 404 }));
+let data;
+
+self.addEventListener("fetch", (event) => {
+  function reject() {
+    if (event.request.headers.get("Accept")?.includes("text/html")) {
+      event.respondWith(new Response('Page not accessible, please contact administrator.', { status: 404 }));
+    } else {
+      event.respondWith(new Response('', { status: 404 }));
+    }
+  }
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data.type === 'LOCAL_STORAGE_KEY') {
+    data = event.data.data;
   }
 });
