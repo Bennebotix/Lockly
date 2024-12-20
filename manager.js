7dd7d4f82fb9812fcc985c85d0f81d00d3a9f147
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-20 10:55:41 -0600

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,1 +1,26 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Installed');
+});
 
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+    const checkOtherWorker = async () => {
+        const registration = await navigator.serviceWorker.getRegistration('/data/');
+        
+        if (registration) {
+            console.log('Another Service Worker is registered at /data/. Redirecting...');
+            event.respondWith(
+                Response.redirect('/data/index.html')
+            );
+        } else {
+            console.log('No Service Worker found at /data/');
+        }
+    };
+
+    if (event.request.url.endsWith('/')) {
+        event.respondWith(checkOtherWorker());
+    }
+});
