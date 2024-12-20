28c92a5a55f8c99050b109dcc0e9afdadf8e09c6
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2024-12-20 11:59:01 -0600

    Update lockly-encryptor.js

--- public/encryptor/lockly-encryptor.js
+++ public/encryptor/lockly-encryptor.js
@@ -12,7 +12,7 @@ fileInput.addEventListener("change", async () => {
 
   const files = fileInput.files;
   if (files.length > 1) {
-    //encryptFolder(files);
+    //await encryptFolder(files);
   } else {
     const file = files[0];
     log(`Selected file: ${file.name}`);
@@ -20,7 +20,7 @@ fileInput.addEventListener("change", async () => {
     if (file.name.endsWith(".zip")) {
       await encryptZip(file);
     } else if (file.name.endsWith(".tar.gz")) {
-      await encryptTarGZ(file);
+      //await encryptTarGZ(file);
     } else {
       log("Unsupported file type. ");
     }
@@ -105,94 +105,6 @@ async function encryptZip(file) {
     log(`Error during encryption: ${error.message}`);
   }
 }
-
-async function encryptTarGZ(file, pwd) {
-  async function saveToTar(encryptedData, path) {
-    const tar = new Tar();
-    tar.addFile(path, encryptedData);
-
-    const tarBlob = await tar.generateAsync({ type: "blob" });
-
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = async () => {
-        const compressed = pako.gzip(new Uint8Array(reader.result), { to: 'uint8array' });
-        const compressedBlob = new Blob([compressed], { type: 'application/gzip' });
-        resolve(compressedBlob);
-      };
-      reader.onerror = (error) => {
-        reject(`Error during gzip compression: ${error}`);
-      };
-      reader.readAsArrayBuffer(tarBlob);
-    });
-  }
-
-  const download = async (content) => {
-    try {
-      const url = URL.createObjectURL(content);
-
-      const a = document.createElement("a");
-      a.href = url;
-      a.download = file.name.replace('.tar.gz', '.tar.gz');
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-
-      URL.revokeObjectURL(url);
-    } catch (error) {
-      console.error("Error downloading encrypted TAR file:", error);
-    }
-  };
-
-  try {
-    log("Decompressing...");
-
-    let tarFiles;
-    await file.arrayBuffer()
-      .then(pako.inflate)
-      .then(arr => arr.buffer)
-      .then(untar)
-      .then(files => {
-        tarFiles = files;
-      });
-
-    clearInterval(dotInterval);
-    log("Decompression done!");
-
-    const totalEntries = Object.keys(files).filter(relativePath => !relativePath.endsWith('/')).length;
-    let processedEntries = 0;
-    
-    const encryptedTarBlobs = [];
-
-    for (const [relativePath, entry] of Object.entries(files)) {
-      if (!relativePath.endsWith('/')) {
-        log(`- Encrypting: ${entry.fileName}...`);
-        try {
-          const entryData = await entry.arrayBuffer();
-          const encryptedFile = await encrypt(entryData, pwd);
-          const savedBlob = await saveToTar(encryptedFile.encryptedData, entry.fileName);
-          
-          encryptedTarBlobs.push(savedBlob);
-          log(`  - ${entry.fileName} encrypted successfully.`);
-        } catch (entryError) {
-          log(`  - Error processing ${entry.fileName}: ${entryError.message}`);
-        }
-        processedEntries++;
-        log(`Progress: ${processedEntries} of ${totalEntries}`);
-      }
-    }
-
-    log("Encryption complete!");
-
-    const finalBlob = new Blob(encryptedTarBlobs);
-
-    await download(finalBlob);
-  } catch (error) {
-    log(`Error during encryption: ${error.message}`);
-  }
-}
-
-
 
 async function encrypt(data, password) {
   const enc = new TextEncoder();
